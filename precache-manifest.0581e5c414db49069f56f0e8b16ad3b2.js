fc9d68c351597e748eb8fa7cd6e1e0daa6e2073b
Brian Adams <brian.adams@clevertech.biz>
2020-08-16 23:21:59 -0400

    initial commit

---
+++ precache-manifest.0581e5c414db49069f56f0e8b16ad3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dce290f6de6910578b733ff8f0b2dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d9122f5e0a80099cb88",
+    "url": "/static/css/main.f24ea461.chunk.css"
+  },
+  {
+    "revision": "ee9cb6a4af76cd33ad4b",
+    "url": "/static/js/2.77342c6c.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.77342c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9122f5e0a80099cb88",
+    "url": "/static/js/main.2b3a5a49.chunk.js"
+  },
+  {
+    "revision": "36891a80d3376be20103",
+    "url": "/static/js/runtime-main.8bc11a17.js"
+  }
+]);
